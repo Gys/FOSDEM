7425c7244c09e84db98cb7511faaa90866b31d5f
Gijs Reulen <greulen@gmail.com>
2021-02-05 19:18:54 +0000

    write list of events to html file

--- main.go
+++ main.go
@@ -4,21 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sort"
 	"strings"
+	"text/template"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 type eventDetails struct {
-	titleHTML       string
-	speakersHTML    string
-	roomHTML        string
-	start           time.Time
-	end             time.Time
-	attachmentsHTML string
-	videoHTML       string
+	TitleHTML       string
+	SpeakersHTML    string
+	RoomHTML        string
+	Start           time.Time
+	End             time.Time
+	AttachmentsHTML string
+	VideoHTML       string
+}
+
+func (e eventDetails) StartAsHTML() string {
+	return e.Start.Format("15:04") + "&nbsp;-&nbsp;" + e.End.Format("15:04")
 }
 
 func getSchedule() (list []eventDetails) {
@@ -51,19 +57,19 @@ func getSchedule() (list []eventDetails) {
 		dt.Each(func(j int, e *goquery.Selection) {
 			// Title with link
 			if j == 0 {
-				event.titleHTML = htmlWithFullUrls(e)
-				// Or maybe split text and link
+				event.TitleHTML = htmlWithFullUrls(e)
+				// TODO: maybe split text and link
 				// event.title = e.Text()
 				// link, _ := e.Find("a").Attr("href")
 				// event.link = "https://fosdem.org" + link
 			}
 			// Speakers
 			if j == 1 {
-				event.speakersHTML = htmlWithFullUrls(e)
+				event.SpeakersHTML = htmlWithFullUrls(e)
 			}
 			// Room
 			if j == 2 {
-				event.roomHTML = htmlWithFullUrls(e)
+				event.RoomHTML = htmlWithFullUrls(e)
 			}
 			// Day
 			if j == 3 {
@@ -71,8 +77,8 @@ func getSchedule() (list []eventDetails) {
 				if e.Text() == "Sunday" {
 					v = "2021-02-07 WET"
 				}
-				event.start, _ = time.Parse("2006-01-02 MST", v)
-				event.end = event.start
+				event.Start, _ = time.Parse("2006-01-02 MST", v)
+				event.End = event.Start
 			}
 			// Start
 			if j == 4 {
@@ -81,7 +87,7 @@ func getSchedule() (list []eventDetails) {
 					d = strings.Replace(d, ":", "h", -1)
 					d += "m"
 					dd, _ := time.ParseDuration(d)
-					event.start = event.start.Add(dd)
+					event.Start = event.Start.Add(dd)
 				}
 			}
 			// End
@@ -91,16 +97,16 @@ func getSchedule() (list []eventDetails) {
 					d = strings.Replace(d, ":", "h", -1)
 					d += "m"
 					dd, _ := time.ParseDuration(d)
-					event.end = event.end.Add(dd)
+					event.End = event.End.Add(dd)
 				}
 			}
 			// Attachments
 			if j == 6 {
-				event.attachmentsHTML = htmlWithFullUrls(e)
+				event.AttachmentsHTML = htmlWithFullUrls(e)
 			}
 			// Video
 			if j == 7 {
-				event.videoHTML = htmlWithFullUrls(e)
+				event.VideoHTML = htmlWithFullUrls(e)
 			}
 		})
 		list = append(list, event)
@@ -110,11 +116,22 @@ func getSchedule() (list []eventDetails) {
 
 func main() {
 	list := getSchedule()
+	// Sort by datetime start
 	sort.Slice(list, func(i int, j int) bool {
-		return list[i].start.Before(list[j].start)
+		return list[i].Start.Before(list[j].Start)
 	})
 	for _, event := range list {
-		fmt.Printf("%s %s\n", event.titleHTML, event.start)
+		fmt.Printf("%s %s\n", event.TitleHTML, event.Start)
+	}
+	fn := "fosdem_schedule.html"
+	f, err := os.Create(fn)
+	if err != nil {
+		log.Fatalf("Unable to create '%s' because %s", fn, err)
+	}
+	defer f.Close()
+	t := template.Must(template.New("").Parse(pageTemplate))
+	if err := t.Execute(f, &list); err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -123,6 +140,34 @@ func htmlWithFullUrls(s *goquery.Selection) string {
 	if err != nil {
 		return ""
 	}
-	html = strings.ReplaceAll(html, "href=\"", "href=\"https://fosdem.org")
+	// Assumption: if one link without the domain then no links have a domain (because attachments seem to have a full link)
+	if !strings.Contains(html, "https://fosdem.org") {
+		html = strings.ReplaceAll(html, "href=\"", "href=\"https://fosdem.org")
+	}
 	return html
 }
+
+const pageTemplate = `
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>The HTML5 Herald</title>
+		<link media="all" rel="stylesheet" type="text/css" href="https://fosdem.org/2021/assets/style/fosdem-18736d187ceb9d8deb0e21312ca92ecbafa3786eabacf5c3a574d0f73c273843.css">
+		<style>
+			#main {
+				max-width: 100%;
+			}
+		</style>
+	</head>
+	<body class="schedule-events">
+		<div id="main">
+			<table class="table table-striped table-bordered table-condensed">
+			{{range .}}
+			<tr><td>{{.StartAsHTML}}</td><td>{{.RoomHTML}}</td><td>{{.TitleHTML}}</td><td>{{.AttachmentsHTML}}</td><td>{{.SpeakersHTML}}</td><td>{{.VideoHTML}}</td></tr>
+			{{end}}
+			</table>
+		</div>
+	</body>
+</html>
+`
